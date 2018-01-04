7daf330354d8b4e3f60deb7e47f4145be259a69b
MRBeussink <MRBeussink@gmail.com>
2018-01-04 09:50:54 -0600

    Initial Commit

---
+++ Scorecard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scorecard
+//
+//  Created by Mark Beussink on 20180104.
+//  Copyright © 2018 Mark Beussink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
